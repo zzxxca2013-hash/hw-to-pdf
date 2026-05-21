e44baa71a2f94efccd50b781cee0da0318ac4e7d
zzxxca2013-hash <zzxxca2013@gmail.com>
2026-05-21 07:06:17 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/hw-to-pdf/sw.js', { scope: '/hw-to-pdf/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
